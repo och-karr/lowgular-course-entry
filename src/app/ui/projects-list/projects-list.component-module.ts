9fdb88d6bbcd2e1a0efd29abd572ed24598beea5
och-karr <okaa93@gmail.com>
2022-11-15 23:42:27 +0100

    feat: display the projects list

--- src/app/ui/projects-list/projects-list.component-module.ts
+++ src/app/ui/projects-list/projects-list.component-module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ProjectsListComponent } from './projects-list.component';
 
 @NgModule({
-  imports: [],
+  imports: [CommonModule],
   declarations: [ProjectsListComponent],
   providers: [],
   exports: [ProjectsListComponent]
