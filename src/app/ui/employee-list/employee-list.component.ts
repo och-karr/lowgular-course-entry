a5cccf218cc0fadbf9a9b584d80f5b6fee1fa8e7
och-karr <okaa93@gmail.com>
2022-11-15 21:55:03 +0100

    refactor: add service and improve employee list component

--- src/app/ui/employee-list/employee-list.component.ts
+++ src/app/ui/employee-list/employee-list.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { EmployeeService } from '../../services/employee.service';
 import { EmployeeModel } from '../../model/employee.model';
 
 @Component({
@@ -10,7 +10,7 @@ import { EmployeeModel } from '../../model/employee.model';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EmployeeListComponent {
-  constructor(private _httpClient: HttpClient) {}
+  constructor(private _employeeService: EmployeeService) { }
   companyName: string = 'Company ABC';
-  data$: Observable<EmployeeModel[] | null> = this._httpClient.get<EmployeeModel[]>('/assets/data/employees.json');
+  data$: Observable<EmployeeModel[] | null> = this._employeeService.getAll();
 }
