c9f5af3e6dfab1f7e35aea202119f5961e7529f8
och-karr <okaa93@gmail.com>
2022-11-16 12:08:05 +0100

    feat: add info about new employee, conditionally visible

--- src/app/ui/employee-form/employee-form.component-module.ts
+++ src/app/ui/employee-form/employee-form.component-module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { EmployeeFormComponent } from './employee-form.component';
+import {CommonModule} from "@angular/common";
 
 @NgModule({
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   declarations: [EmployeeFormComponent],
   providers: [],
   exports: [EmployeeFormComponent]
