4f85ac7240924db8e5ebdc23df69a19030abc695
och-karr <okaa93@gmail.com>
2022-11-16 22:48:39 +0100

    feat: pass the form values and display it after submit

--- src/app/ui/employee-form/employee-form.component.ts
+++ src/app/ui/employee-form/employee-form.component.ts
@@ -14,7 +14,7 @@ export class EmployeeFormComponent {
     age: new FormControl(null, Validators.min(0))
   });
 
-  onButtonClicked() {
-    alert('User was successfully added to thedatabase');
+  onButtonClicked(form: {name: string, salary: string, age: string}) {
+    alert('User was successfully added to the database. Name: ' + form.name + ', Salary: '+ form.salary + ', Age:' + form.age);
   }
 }
