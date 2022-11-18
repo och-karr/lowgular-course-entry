1abe8102dce099e106504ff57717751d5dc5a709
och-karr <okaa93@gmail.com>
2022-11-18 15:21:28 +0100

    feat: display employees from new api

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,3 +1,5 @@
+import { ApiResponse } from "./api.response";
+import { EmployeeResponse } from "./employee.response";
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
@@ -9,8 +11,25 @@ export class EmployeeService {
   constructor(private _httpClient: HttpClient) {
   }
 
+  // getAllPeople(): Observable<PersonModel[]> {
+  //   return this._httpClient.get<PersonModel[]>('/assets/data/people.json');
+  // }
+
   getAll(): Observable<PersonModel[]> {
-    return this._httpClient.get<PersonModel[]>('/assets/data/people.json');
+    return this._httpClient.get<ApiResponse<EmployeeResponse[]>>(
+      'https://dummy.restapiexample.com/api/v1/employees',
+    ).pipe(
+      map((response: ApiResponse<EmployeeResponse[]>) => {
+        return response.data.map((employeeResponse: EmployeeResponse) => {
+          return {
+            personalNumber: employeeResponse.id,
+            name: employeeResponse.employee_name,
+            mail: (employeeResponse.employee_name).replace(/\s/g, "").toLowerCase() + "@lowgular.io",
+            img: employeeResponse.profile_image
+          }
+        });
+      })
+    )
   }
 
   create(employee: CreateEmployeeModel): Observable<any> {
