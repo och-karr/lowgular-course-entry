009a09d949198f4105e620063a07f8bedc1c4b48
och-karr <okaa93@gmail.com>
2022-11-25 10:09:43 +0100

    feat: add routing to create employee component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmployeeListComponent } from './ui/employee-list/employee-list.component';
+import { EmployeeFormComponent } from './ui/employee-form/employee-form.component';
 import { EmployeeListComponentModule } from './ui/employee-list/employee-list.component-module';
 import { EmployeeServiceModule } from './services/employee.service-module';
+import { EmployeeFormComponentModule } from './ui/employee-form/employee-form.component-module';
 
-const routes: Routes = [{ path: '', component: EmployeeListComponent }];
+const routes: Routes = [{ path: '', component: EmployeeListComponent }, { path: 'create-employee', component: EmployeeFormComponent }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), EmployeeListComponentModule, EmployeeServiceModule],
+  imports: [RouterModule.forRoot(routes), EmployeeListComponentModule, EmployeeServiceModule, EmployeeFormComponentModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
