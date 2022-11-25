4380c1b5be79ed6087de03ff90812b99c7182a44
och-karr <okaa93@gmail.com>
2022-11-25 10:02:31 +0100

    feat: display only employee list under main page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeeListComponent } from './ui/employee-list/employee-list.component';
+import { EmployeeListComponentModule } from './ui/employee-list/employee-list.component-module';
+import { EmployeeServiceModule } from './services/employee.service-module';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: EmployeeListComponent }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), EmployeeListComponentModule, EmployeeServiceModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
