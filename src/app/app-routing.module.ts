a0d90aead26276a11ba2fe062506ad241558cb0b
och-karr <okaa93@gmail.com>
2022-11-25 11:03:56 +0100

    feat and refactor: change route path for employee list and add home component with anchors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmployeeListComponent } from './ui/employee-list/employee-list.component';
 import { EmployeeFormComponent } from './ui/employee-form/employee-form.component';
+import { HomeComponent } from './ui/home/home.component';
 import { EmployeeListComponentModule } from './ui/employee-list/employee-list.component-module';
 import { EmployeeServiceModule } from './services/employee.service-module';
 import { EmployeeFormComponentModule } from './ui/employee-form/employee-form.component-module';
+import { HomeComponentModule } from './ui/home/home.component-module';
 
-const routes: Routes = [{ path: '', component: EmployeeListComponent }, { path: 'create-employee', component: EmployeeFormComponent }];
+const routes: Routes = [
+  { path: 'employee-list', component: EmployeeListComponent },
+  { path: 'create-employee', component: EmployeeFormComponent },
+  { path: '', component: HomeComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), EmployeeListComponentModule, EmployeeServiceModule, EmployeeFormComponentModule],
+  imports: [RouterModule.forRoot(routes), EmployeeListComponentModule, EmployeeServiceModule, EmployeeFormComponentModule, HomeComponentModule],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
