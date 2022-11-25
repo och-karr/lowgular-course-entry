7e6b404fdce35a13674125c422df63293b57bc4b
och-karr <okaa93@gmail.com>
2022-11-25 23:06:39 +0100

    feat: display name in employee details page

--- src/app/ui/employee-details/employee-details.component.ts
+++ src/app/ui/employee-details/employee-details.component.ts
@@ -12,7 +12,10 @@ import {EmployeeDetailsParamsModel} from "../../model/employee-details-params.mo
 export class EmployeeDetailsComponent {
   // readonly params$: Observable<EmployeeDetailsParamsModel> = this._activatedRoute.params as Observable<EmployeeDetailsParamsModel>;
   readonly params$: Observable<EmployeeDetailsParamsModel> = this._activatedRoute.params.pipe(
-    map(params => ({ id: params['id']}))
+    map(params => ({
+      id: params['id'],
+      name: params['name']
+    }))
   )
   constructor(private _activatedRoute: ActivatedRoute) {
   }
